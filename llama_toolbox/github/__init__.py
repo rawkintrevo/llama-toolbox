d73b4285f085564d7cacba410211bed64f1a5e7b
rawkintrevo <trevor.d.grant@gmail.com>
2024-12-11 14:46:47 -0600

    update init

--- llama_toolbox/github/__init__.py
+++ llama_toolbox/github/__init__.py
@@ -3,6 +3,7 @@ from.create_issue import CreateIssue
 from.get_repo_contents import GetRepoContents
 from.commit_file import CommitFile
 from.search import SearchRepos
+from .read_issue import ReadIssue
 
 
 
