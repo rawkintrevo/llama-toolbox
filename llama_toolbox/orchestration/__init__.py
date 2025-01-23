040af08ed4de06cbe6569957e687dbac4617bb2c
rawkintrevo <trevor.d.grant@gmail.com>
2025-01-23 11:41:25 -0600

    pass model_name

--- llama_toolbox/orchestration/__init__.py
+++ llama_toolbox/orchestration/__init__.py
@@ -12,13 +12,13 @@ class FunctionOrchestrator:
         self.function_map = {f['function']['name']: FunctionRegistry._tools[f['function']['name']]
                              for f in self.available_functions}
 
-    def execute_workflow(self, user_query, max_steps=5):
+    def execute_workflow(self, user_query:str, model_name:str, max_steps=5):
         messages = [{"role": "user", "content": user_query}]
 
         for _ in range(max_steps):
             # Get LLM response with function calls
             response = self.llm.chat.completions.create(
-                model="meta-llama/Llama-3-70B-Instruct",
+                model=model_name,
                 messages=messages,
                 functions=self.available_functions
             )
